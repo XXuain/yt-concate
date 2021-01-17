b1abecdf4abce56541d028f5882c8c4086832a3c
yellowHuang <hlady1115@gmail.com>
2021-01-17 14:04:00 +0800

    catch pytube error & check file exit

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -2,23 +2,32 @@ import ssl
 
 # No ssl verification
 ssl._create_default_https_context = ssl._create_unverified_context
+
+from yt_concate.pipeline.steps.preflight import Preflight
+from yt_concate.pipeline.steps.postflight import Postflight
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
+from yt_concate.pipeline.steps.download_captions import DownLoadCaptions
 from yt_concate.pipeline.pipeline import Pipeline
+from yt_concate.utils import Utils
 
 CHANNEL_ID = 'UCKSVUHI9rbbkXhvAXK-2uxA'
 
 
 def main():
     steps = [
+        Preflight(),
         GetVideoList(),
+        DownLoadCaptions(),
+        Postflight()
     ]
 
     inputs = {
         'channel_id': CHANNEL_ID
     }
 
+    utils = Utils()
     p = Pipeline(steps)
-    p.run(inputs)
+    p.run(inputs, utils)
 
 
 if __name__ == '__main__':
--- yt_concate/pipeline/steps/get_video_list.py
+++ yt_concate/pipeline/steps/get_video_list.py
@@ -6,12 +6,18 @@ from yt_concate.pipeline.steps.step import Step
 
 
 class GetVideoList(Step):
-    def process(self, data, inputs):
+    def process(self, data, inputs, utils):
         channel_id = inputs['channel_id']
+
+        # 檢查
+        if utils.video_list_file_exits(channel_id):
+            print('Check existing video list file for channel id', channel_id)
+            return self.read_to_file(utils.get_video_list_filePath(channel_id))
+
+        # 下載
         api_key = API_KEY
         base_video_url = 'https://www.youtube.com/watch?v='
         base_search_url = 'https://www.googleapis.com/youtube/v3/search?'
-
         first_url = base_search_url + 'key={}&channelId={}&part=snippet,id&order=date&maxResults=25'.format(api_key,
                                                                                                             channel_id)
 
@@ -30,5 +36,20 @@ class GetVideoList(Step):
                 url = first_url + '&pageToken={}'.format(next_page_token)
             except KeyError:
                 break
+
         print(video_links)
+        self.write_to_file(video_links, utils.get_video_list_filePath(channel_id))
+        return video_links
+
+    def write_to_file(self, vedio_links, file_path):
+        with open(file_path, 'w') as f:
+            for url in vedio_links:
+                f.write(url + '\n')
+
+    def read_to_file\
+                    (self, file_path):
+        video_links = []
+        with open(file_path, 'r') as f:
+            for url in f:
+                video_links.append(url.strip())
         return video_links
