cee166e587230fddea404e23acf9f7f77b0ca66c
yellowHuang <hlady1115@gmail.com>
2021-01-24 15:20:42 +0800

    new yt model

--- yt_concate/utils.py
+++ yt_concate/utils.py
@@ -21,17 +21,9 @@ class Utils:
         path = self.get_video_list_filePath(channel_id)
         return os.path.exists(path) and os.path.getsize(path) > 0
 
-    @staticmethod
-    def get_video_id_from_url(url):
-        return url.split('watch?v=')[-1]
-
-    # 用 id 命名為字幕檔案名稱
-    def get_caption_path(self, url):
-        return os.path.join(CAPTIONS_DIR, self.get_video_id_from_url(url) + '.txt')
-
     # 檢查檔案是否存在
-    def caption_file_exists(self, url):
-        path = self.get_caption_path(url)
-        os.path.exists(path) and os.path.getsize(path) > 0
+    def caption_file_exists(self, yt):
+        filePath = yt.caption_filePath
+        os.path.exists(filePath) and os.path.getsize(filePath) > 0
 
 
