b32722a68d0a7f403d0a835c43f147696f862fd3
yellowHuang <hlady1115@gmail.com>
2021-01-24 15:21:55 +0800

    new initialize step

--- yt_concate/main.py
+++ yt_concate/main.py
@@ -2,9 +2,11 @@ import ssl
 
 # No ssl verification
 ssl._create_default_https_context = ssl._create_unverified_context
+
 from yt_concate.pipeline.steps.preflight import Preflight
 from yt_concate.pipeline.steps.get_video_list import GetVideoList
-# from yt_concate.pipeline.steps.download_captions import DownLoadCaptions
+from yt_concate.pipeline.steps.initializeYT import InitializeYT
+from yt_concate.pipeline.steps.download_captions import DownLoadCaptions
 from yt_concate.pipeline.steps.read_caption import ReadCaption
 from yt_concate.pipeline.steps.postflight import Postflight
 from yt_concate.pipeline.pipeline import Pipeline
@@ -17,13 +19,15 @@ def main():
     steps = [
         Preflight(),
         GetVideoList(),
-        # DownLoadCaptions(),
+        InitializeYT(),
+        DownLoadCaptions(),
         ReadCaption(),
         Postflight()
     ]
 
     inputs = {
-        'channel_id': CHANNEL_ID
+        'channel_id': CHANNEL_ID,
+        'search_word': 'incredible',
     }
 
     utils = Utils()
